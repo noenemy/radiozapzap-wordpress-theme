331b416d73ad7388981613505b4eef69fdc96a6d
noenemy <noenemy@gmail.com>
2020-10-01 19:25:22 +0900

    Add front-page.php and single.php

--- index.php
+++ index.php
@@ -1,137 +1,38 @@
-<?php get_header(); ?>
+<?php
 
+    get_header(); ?>
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/site-main-bg-01.jpg'); ?>);"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h2 class="headline headline--medium"><strong>radio</strong>zapzap</h2>
-        <p></p>
-        <h3 class="headline headline--small">아직은 뭐라고 정의하기 어려운 가내수공업 밴드</h3>
-        <a href="#" class="btn btn--large btn--blue">Learn more</a>
-      </div>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/stage.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <div class="page-banner__intro">
+            <p>Keep up with our latest news.</p>
+        </div>
+        </div>  
     </div>
 
-    <div class="full-width-split group">
-      <div class="full-width-split__one">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">Recent Projects</h2>
-
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">High and Dry</a></h5>
-              <p>1995년에 발표된 라디오헤드의 The Bends 앨범 수록곡. 수년 째 커버를 하고 있지만 아직 갈 길이 멀다.<a href="#" class="nu gray">Learn more</a></p>
-            </div>
+    <div class="container container--narrow page-section">
+    <?php 
+      while(have_posts()) {
+          the_post(); ?>
+        <div class="post-item">
+          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <div class="metabox">
+            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
           </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">No Surprises</a></h5>
-              <p>1997년 OK Computer의 4번째 싱글 발표곡이다. 뮤직비디오에서 톰요크 형님의 숨넘어 갈 듯한 섬세한 표정 연기가 압권이다. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Fake Plastic Trees</a></h5>
-              <p>1995년 The Bends 앨범 수록곡. 짭짭을 모토로하는 라디오짭짭의 정신세계를 노래한 듯 하다.<a href="#" class="nu gray">Learn more</a></p>
-            </div>
+          <div class="generic-content">
+            <?php the_excerpt(); ?>
+            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
           </div>          
-
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Projects</a></p>
         </div>
-      </div>
-      <div class="full-width-split__two">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Jan</span>
-              <span class="event-summary__day">20</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">프롤로그: From the Basement</a></h5>
-              <p>라디오헤드에 미쳐있는 후배 녀석과 함께 작년에 Tribute 밴드를 하나 만들었습니다. 10년전에 직장 밴드 동호회에서 만난 이 친구는 일렉기타를 맛깔 나게 치는 친구였는데, 2012년 지산락페에서 <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">그 밴드의 시작</a></h5>
-              <p>뜻밖의 부재중 전화 2통… 예전에 알던 분으로부터 부재중 전화가 와있다.전화를 할까 말까 잠시 고민한다. 모처럼의 제주도 나홀로 여행이라 <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">처음의 음악 #1 리듬 정복</a></h5>
-              <p>정석대로라면 오선지를 마디로 나누어 1234 분석해가며 리듬 청음 연습을 해야겠지만 (청음이란 악보없이 들으면서 멜로디/화성/리듬을 악보로 적는 것을 말합니다. 아마추어가 어느 세월에 <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>          
-
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
-        </div>
-      </div>
+      <?php } 
+    
+      echo paginate_links();
+    ?>
     </div>
 
-    <div class="hero-slider">
-      <div data-glide-el="track" class="glide__track">
-        <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/radio.jpg'); ?>">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Radiohead Tribute Band</h2>
-                <p class="t-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/tape.jpg'); ?>">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Home Recording</h2>
-                <p class="t-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/livestreaming.jpg'); ?>">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Live Streaming</h2>
-                <p class="t-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/writing.jpg'); ?>">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Team Blogging</h2>
-                <p class="t-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>          
-        </div>
-        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
-      </div>
-    </div>
 
-<?php get_footer(); ?>
+<?php
+    get_footer();
+?>
