43d2241ba95b674ac6a8adc2988a2856776d05c6
noenemy <noenemy@gmail.com>
2020-10-03 18:57:00 +0900

    Add template-part feature

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
         <h2 class="headline headline--medium"><strong>radio</strong>zapzap</h2>
         <p></p>
         <h3 class="headline headline--small">아직은 뭐라고 정의하기 어려운 가내수공업 밴드</h3>
-        <a href="<?php echo site_url('/about-us'); ?>" class="btn btn--large btn--blue">Learn more</a>
+        <a href="<?php echo site_url('/about-us'); ?>" class="btn btn--medium btn--blue">Learn more</a>
       </div>
     </div>
 
@@ -25,25 +25,11 @@
             ));
             
             while ($recentProjects->have_posts()) {
-              $recentProjects->the_post(); ?>
-          
-            <div class="event-summary">
-              <a class="event-summary__date event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php the_time('M'); ?></span>
-                <span class="event-summary__day"><?php the_time('d'); ?></span>
-              </a>
-              <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php if (has_excerpt()) {
-                echo get_the_excerpt();
-              } else {
-                echo wp_trim_words(get_the_content(), 20);
-              }  
-              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-              </div>
-            </div>
-
-            <?php } ?>      
+              $recentProjects->the_post(); 
+              get_template_part('template-parts/content', 'event'); 
+            } 
+            wp_reset_postdata();
+            ?>      
 
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('project'); ?>" class="btn btn--blue">View All Projects</a></p>
         </div>
@@ -59,26 +45,11 @@
             ));
 
             while ($newBlogPosts->have_posts()) {
-              $newBlogPosts->the_post(); ?>
-
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month"><?php the_time('M'); ?></span>
-              <span class="event-summary__day"><?php the_time('d'); ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-              <p><?php if (has_excerpt()) {
-                echo get_the_excerpt();
-              } else {
-                echo wp_trim_words(get_the_content(), 20);
-              }  
-              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-
-          <?php } wp_reset_postdata();
-          ?>      
+              $newBlogPosts->the_post();
+              get_template_part('template-parts/content', 'post'); 
+            } 
+            wp_reset_postdata();
+            ?>     
 
           <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
